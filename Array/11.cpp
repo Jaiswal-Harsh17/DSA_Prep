15c5acbee0b013135a3cdf1436e2160fa24b2891
Harsh Jaiswal <harshjais4398@gmail.com>
2025-11-09 23:57:24 +0530

    Leetcode 2169

--- Array/11.cpp
+++ Array/11.cpp
@@ -14,3 +14,22 @@
 //         return totalTime;
 //     }
 // };
+
+// Leetcode - 2169. Count Operations to Obtain Zero
+// class Solution {
+// public:
+//     int countOperations(int num1, int num2) {
+//         int cnt = 1;
+//         if(num1 != num2){
+//             if(num1 >= num2){
+//                 num1 = num1 - num2;
+//                 cnt++;
+//             }
+//             if(num2 >= num1){
+//                 num2 = num2 - num1;
+//                 cnt++;
+//             }
+//         }
+//         return cnt;
+//     }
+// };
