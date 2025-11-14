18cab87f99ceab35158306dd3a781961af9955e7
Harsh Jaiswal <harshjais4398@gmail.com>
2025-11-14 22:37:35 +0530

    Sum of prime code

---
+++ Array/Sum of Prime.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool isPrime(int num){
+    for(int i=2;i<num;i++){
+        if(num % i == 0){
+            return false;
+        }
+    }
+    return true;
+}
+int main(){
+    int n;
+    cout<<"Enter N: ";
+    cin>>n;
+    int sum = 0;
+    for(int i=2;i<=n;i++){
+        if(isPrime(i)){
+            sum += i;
+        }
+    }
+    cout<<sum;
+    return 0;
+}
