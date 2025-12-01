b0ac1375b3ce6903ba7d5b8372e5794937157e8d
Harsh Jaiswal <harshjais4398@gmail.com>
2025-12-01 23:10:06 +0530

    Problem 2446 Leetcode

--- New_P.cpp
+++ New_P.cpp
@@ -1,1 +1,13 @@
-// Leetcode Daily Problrms
+// Leetcode Daily Problems
+// 01/12/2025
+
+// 2446. Determine if Two Events Have Conflict
+// Code:
+// class Solution {
+// public:
+//     bool haveConflict(vector<string>& event1, vector<string>& event2) {
+//         string st1 = event1[0], et1 = event1[1], st2 = event2[0], et2 = event2[1];
+//         if((et1 < st2) || (st1 > et2)) return false;
+//         else return true;
+//     }
+// };
