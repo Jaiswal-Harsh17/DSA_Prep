f598b1c299317d7128418559f81b7e3016737f72
Harsh Jaiswal <harshjais4398@gmail.com>
2025-12-05 23:57:00 +0530

    Leetcode Problem No. 3234

--- New_P.cpp
+++ New_P.cpp
@@ -52,3 +52,18 @@
 //         return count;
 //     } 
 // };
+
+// 3432. Count Partitions with Even Sum Difference
+// Code:
+// class Solution {
+// public:
+//     int countPartitions(vector<int>& nums) {
+//         int total = 0;
+//         for(int num:nums){
+//             total += num;
+//         }
+//         int n = nums.size();
+//         if(total % 2 != 0) return 0;
+//         else return n-1;
+//     }
+// };
