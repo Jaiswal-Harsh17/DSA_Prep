77410bb3f24539964630291b966d57571c4488de
Harsh Jaiswal <harshjais4398@gmail.com>
2025-12-06 23:55:04 +0530

    Leetcode Problem No. 1141

--- New_P.cpp
+++ New_P.cpp
@@ -67,3 +67,11 @@
 //         else return n-1;
 //     }
 // };
+
+// 1141. User Activity for the Past 30 Days I
+// Code:
+// # Write your MySQL query statement below
+// SELECT activity_date AS day, COUNT(DISTINCT user_id) AS active_users
+// FROM Activity
+// WHERE (activity_date > "2019-06-27" AND activity_date <= "2019-07-27")
+// GROUP BY activity_date;
