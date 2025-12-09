a5d39af556bcd9615ed4d162773889fdef55dc33
Harsh Jaiswal <harshjais4398@gmail.com>
2025-12-09 23:57:30 +0530

    Leetcode Problem No. 3583

--- New_P.cpp
+++ New_P.cpp
@@ -75,3 +75,27 @@
 // FROM Activity
 // WHERE (activity_date > "2019-06-27" AND activity_date <= "2019-07-27")
 // GROUP BY activity_date;
+
+// 3583. Count Special Triplets
+// Code:
+// int specialTriplets(vector<int>& nums) {
+//     const int mod = 1e9 + 7;
+//     int n   = nums.size();
+//     int ans = 0;
+//     for(int j = 0; j < n; j ++){
+//         int Lcount = 0;
+//         for(int i = 0; i < j; i ++){
+//             if(nums[i] == nums[j] * 2){
+//                 Lcount += 1;
+//             }
+//         }
+//         int Rcount = 0;
+//         for(int k = j + 1; k < n; k ++){
+//             if(nums[k] == nums[j] * 2){
+//                 Rcount += 1;
+//             }
+//         }
+//         ans += (long long)Lcount * Rcount;
+//     }
+//     return ans % mod;
+// }
