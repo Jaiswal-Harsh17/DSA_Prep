e90bc771ec0502848e60360a20c3a7e3a422fac8
Harsh Jaiswal <harshjais4398@gmail.com>
2025-12-16 23:50:33 +0530

    Leetcode Problem No. 1148

--- New_P.cpp
+++ New_P.cpp
@@ -121,3 +121,8 @@
 //           return res;
 //     }
 // };
+
+// 1148. Article Views I
+// Code:
+// # Write your MySQL query statement below
+// Select distinct viewer_id as id from Views where author_id = viewer_id order by id;  
