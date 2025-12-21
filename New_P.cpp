100477b5eeff29781e26bb84bb1baa765237f31c
Harsh Jaiswal <harshjais4398@gmail.com>
2025-12-21 23:53:36 +0530

    Leetcode Problem No. 1318

--- New_P.cpp
+++ New_P.cpp
@@ -203,3 +203,22 @@
 //         return res;
 //     }
 // };
+
+// 1318. Minimum Flips to Make a OR b Equal to c
+// Code
+// class Solution {
+// public:
+//     int minFlips(int a, int b, int c) {
+//         int cnt = 0;
+//         while (a != 0 || b != 0 || c != 0) {
+//             if (a & 1 && !(c & 1)) cnt++;
+//             if (b & 1 && !(c & 1)) cnt++;
+//             if (!(a & 1) && !(b & 1) && (c & 1)) cnt++;
+//             a >>= 1;
+//             b >>= 1;
+//             c >>= 1;
+//         }
+//         return cnt;
+//     }
+// };
+
