7f8c13d22ae19ed4eba704e66e4ba16727c43413
Harsh Jaiswal <harshjais4398@gmail.com>
2025-12-29 23:52:00 +0530

    Leetcode Problem No. 2441

--- New_P.cpp
+++ New_P.cpp
@@ -253,3 +253,23 @@
 //         return count;
 //     }
 // };
+
+// 2441. Largest Positive Integer That Exists With Its Negative
+// Code:
+// class Solution {
+// public:
+//     int findMaxK(vector<int>& nums) 
+//     {
+//         sort(nums.begin(), nums.end());
+//         int low=0, high=nums.size()-1;
+//         while(low < high){
+//             if((nums[low] + nums[high]) == 0){
+//                 return  nums[high];
+//             }
+//             else if((nums[low] + nums[high]) < 0)
+//                 low++;
+//             else high--;
+//         }
+//         return -1;
+//     }
+// };
