15b501a8d7224c55aa969a27e7e1a28089c9fd21
Harsh Jaiswal <harshjais4398@gmail.com>
2026-01-01 23:55:53 +0530

    Leetcode Problem No. 66

--- New_P.cpp
+++ New_P.cpp
@@ -305,3 +305,27 @@
 //         return indexans;
 //     }
 // };
+
+// 66. Plus One
+// code:
+// class Solution {
+// public:
+//     vector<int> plusOne(vector<int>& digits) {
+//         int n = digits.size();
+//         for(int i=n-1;i>=0;i--){
+//             if(i==n-1)
+//                 digits[i]++;
+//             if(digits[i]==10){
+//                 digits[i]=0;
+//                 if(i!=0){
+//                     digits[i-1]++;
+//                 }
+//                 else{
+//                     digits.push_back(0);
+//                     digits[i]=1;
+//                 }
+//             }
+//         }
+//         return digits;
+//     }
+// };
