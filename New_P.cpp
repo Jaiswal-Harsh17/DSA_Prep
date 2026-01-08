b725aee0f4d04677e8a297898c0a23d00556366b
Harsh Jaiswal <harshjais4398@gmail.com>
2026-01-08 20:51:22 +0530

    Leetcode Problem No. 852

--- New_P.cpp
+++ New_P.cpp
@@ -408,3 +408,24 @@
 //         return ans;
 //     }
 // };
+
+// 852. Peak Index in a Mountain Array
+// Code:
+// class Solution {
+// public:
+//     int peakIndexInMountainArray(vector<int>& arr) {
+//     int n=arr.size();
+// 	int l=0,r=n-1,mid;
+// 	while(l<r)
+// 	{
+// 		mid=(l+r)/2;
+// 		if(arr[mid]<arr[mid+1]){
+// 			l=mid+1;
+// 		}
+// 		else{
+// 			r=mid;
+// 		}
+// 	}
+//     return l;
+// }
+// };
