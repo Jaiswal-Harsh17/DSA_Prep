1bb7031e373c97b8192330890a6b15f6e0afae5b
Harsh Jaiswal <harshjais4398@gmail.com>
2026-01-21 23:50:47 +0530

    Leetcode Problem No. 3315

--- New_P.cpp
+++ New_P.cpp
@@ -429,3 +429,24 @@
 //     return l;
 // }
 // };
+
+// 3315. Construct the Minimum Bitwise Array II
+// Code :
+// class Solution{
+// public:
+//     vector<int> minBitwiseArray(vector<int>& nums){
+//         int n=nums.size();
+//         vector<int> ans(n);
+//         for(int i=0;i<n;i++){
+//             int a=nums[i];
+//             int b=a+1;
+//             if(a==2){
+//                 ans[i]=-1;
+//             }
+//             else{
+//                 ans[i]=a-((b)&(-b))/2;
+//             }
+//         }
+//         return ans;
+//     }
+// };
