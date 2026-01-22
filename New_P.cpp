6f728f50d511157ab59e0f463d2735c7fb3dbcda
Harsh Jaiswal <harshjais4398@gmail.com>
2026-01-22 23:59:12 +0530

    Leetcode Problem No. 3507

--- New_P.cpp
+++ New_P.cpp
@@ -450,3 +450,33 @@
 //         return ans;
 //     }
 // };
+
+// 3507. Minimum Pair Removal to Sort Array I
+// Code:
+// class Solution {
+// public:
+//     int minimumPairRemoval(vector<int>& nums) {
+//         int operations = 0;
+//         while(true){
+//             bool isSorted = true;
+//             for(int i = 0; i < nums.size() - 1;i++){
+//                 if(nums[i] > nums[i+1]){
+//                     isSorted = false;
+//                 }
+//             }
+//             if(isSorted) return operations;
+//             int minSum = INT_MAX;
+//             int index;
+//             for(int i = 0; i < nums.size() - 1; i++){
+//                 int currentSum = nums[i] + nums[i+1];
+//                 if(minSum > currentSum){
+//                     minSum = currentSum;
+//                     index = i;
+//                 }
+//             }
+//             nums[index] = minSum;
+//             nums.erase(nums.begin() + index + 1);
+//             operations++;
+//         }
+//     }
+// };
