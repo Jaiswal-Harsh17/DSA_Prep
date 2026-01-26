21204196bbea3ee694aa9f3fd4f79854fa6c28e5
Harsh Jaiswal <harshjais4398@gmail.com>
2026-01-26 23:14:57 +0530

    Leetcode Problem No. 1200

--- New_P.cpp
+++ New_P.cpp
@@ -480,3 +480,23 @@
 //         }
 //     }
 // };
+
+// 1200. Minimum Absolute Difference
+// Code:
+// class Solution {
+// public:
+//     vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+//         sort(arr.begin(), arr.end());
+//         vector<vector<int>>ans;
+//         int min_diff = arr[1] - arr[0];
+//         for(int i=0;i<arr.size()-1;i++){
+//             min_diff = min(min_diff, arr[i+1] - arr[i]);
+//         }   
+//         for(int i=0;i<arr.size()-1;i++){
+//             if(arr[i+1] - arr[i] == min_diff){
+//                 ans.push_back({arr[i], arr[i+1]});
+//             }
+//         }
+//         return ans;
+//     }
+// };
