6720a1fe6d10f641120e0cb5fd0fe46af689a74c
Harsh Jaiswal <harshjais4398@gmail.com>
2026-02-07 23:42:56 +0530

    Leetcode Problem No. 1653

--- New_P.cpp
+++ New_P.cpp
@@ -573,3 +573,20 @@
 
 // 3379. Transformed Array
 // Code
+
+// 1653. Minimum Deletions to Make String Balanced
+// Code:
+// class Solution{
+//     public:
+//         int minimumDeletions(string s){
+//             int res=0, count=0;
+//             for(char c:s){
+//                 if(c=='b') count++;
+//                 else if(count){
+//                     res++;
+//                     count--;
+//                 }
+//             }
+//             return res;
+//         }
+// };
