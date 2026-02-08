a1006d07cb859df6807a581bef1f8b47be4e4b21
Harsh Jaiswal <harshjais4398@gmail.com>
2026-02-08 23:56:40 +0530

    Leetcode Problem No. 110

--- New_P.cpp
+++ New_P.cpp
@@ -590,3 +590,23 @@
 //             return res;
 //         }
 // };
+
+// 110. Balanced Binary Tree
+// Code:
+// class Solution {
+// public:
+//     bool isBalanced(TreeNode* root) {
+//         return dfsHeight(root) != -1;
+//     }
+//     int dfsHeight(TreeNode* root){
+//         if(root == NULL) return 0;
+//         int leftHeight = dfsHeight(root->left);
+//         // cout<<leftHeight<<endl;
+//         if(leftHeight == -1) return -1;
+//         int rightHeight = dfsHeight(root->right);
+//         // cout<<rightHeight<<endl;
+//         if(rightHeight == -1) return -1;
+//         if(abs(leftHeight-rightHeight) > 1) return -1;
+//         return max(leftHeight, rightHeight)+1;
+//     }
+// };
